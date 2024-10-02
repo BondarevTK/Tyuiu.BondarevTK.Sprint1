e45433d3f4ace274338c1959828d054ea3e60437
BondarevTK <BondarevTK101@gmail.com>
2024-10-02 17:26:26 +0300

    Add project files.

---
+++ Tyuiu.BondarevTK.Sprint1.V20.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Tyuiu.BondarevTK.Sprint1.V20.Lib;
+namespace Tyuiu.BondarevTK.Sprint1.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var ds = new DataService();
+            Assert.AreEqual(ds.Calculate(), 13);
+        }
+    }
+}
